9a2cafd490530fbd77c2e91b35feba3a9a681876
burhan <9burhanjehloh@gmail.com>
2024-03-05 16:32:52 +0700

    s

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,9 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/services/api-Publist_service.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   final Map<String, dynamic> data;
 
   const HomePage({Key? key, required this.data}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  late List<Map<String, dynamic>> lightClients;
+  late List<bool> switchStates;
+
+  @override
+  void initState() {
+    super.initState();
+    _initializeSwitchStates();
+  }
+
+  void _initializeSwitchStates() {
+    lightClients = widget.data["userData"]["client"]
+        .where((client) => client["typeClient"] == "Light")
+        .cast<Map<String, dynamic>>()
+        .toList();
+    switchStates = List.generate(lightClients.length, (index) => false);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -13,38 +36,92 @@ class HomePage extends StatelessWidget {
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              'Token: ${data["token"]}',
-              style: TextStyle(fontSize: 18),
-            ),
-            SizedBox(height: 10),
-            Text(
-              'User ID: ${data["userData"]["userId"]}',
-              style: TextStyle(fontSize: 18),
-            ),
-            Text(
-              'Username: ${data["userData"]["username"]}',
-              style: TextStyle(fontSize: 18),
-            ),
-            SizedBox(height: 10),
-            Text(
-              'Client ID: ${data["userData"]["client"]["id"]}',
-              style: TextStyle(fontSize: 18),
-            ),
-            Text(
-              'Created At: ${data["userData"]["client"]["createdAt"]}',
-              style: TextStyle(fontSize: 18),
-            ),
-            Text(
-              'Updated At: ${data["userData"]["client"]["updatedAt"]}',
-              style: TextStyle(fontSize: 18),
-            ),
-          ],
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'User ID: ${widget.data["userData"]["userId"]}',
+                style: TextStyle(fontSize: 18),
+              ),
+              Text(
+                'Username: ${widget.data["userData"]["username"]}',
+                style: TextStyle(fontSize: 18),
+              ),
+              SizedBox(height: 20),
+              Text(
+                'Client Information:',
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              ),
+              SizedBox(height: 10),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: List.generate(lightClients.length, (index) {
+                  final client = lightClients[index];
+                  return Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Client ${index + 1}:',
+                        style: TextStyle(
+                            fontSize: 18, fontWeight: FontWeight.bold),
+                      ),
+                      Text(
+                        'Type: ${client["typeClient"]}',
+                        style: TextStyle(fontSize: 16),
+                      ),
+                      Text(
+                        'ID: ${client["client"]}',
+                        style: TextStyle(fontSize: 16),
+                      ),
+                      SizedBox(height: 10),
+                      Switch(
+                        value: switchStates[index],
+                        onChanged: (newValue) {
+                          setState(() {
+                            switchStates[index] = newValue;
+                          });
+                          String switchData = newValue ? 'on' : 'off';
+                          _sendDataToServer(switchData, client["client"]);
+                        },
+                      ),
+                      SizedBox(height: 10),
+                    ],
+                  );
+                }),
+              ),
+            ],
+          ),
         ),
       ),
     );
+  }
+
+  Future<void> _sendDataToServer(String switchData, String client) async {
+    try {
+      final response = await apiPublist_service.sendDataToServer(
+        switchData: switchData,
+        token: widget.data["token"],
+        clientIndex: client,
+      );
+      if (response.statusCode == 500) {
+        print('Data sent successfully!');
+        _updateSwitchState(client);
+      } else {
+        print('Failed to send data. Status code: ${response.statusCode}');
+      }
+    } catch (error) {
+      print('Error: $error');
+    }
+  }
+
+  void _updateSwitchState(String clientId) {
+    final index =
+        lightClients.indexWhere((client) => client["client"] == clientId);
+    if (index != -1) {
+      setState(() {
+        switchStates[index] = !switchStates[index];
+      });
+    }
   }
 }
