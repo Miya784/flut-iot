44262e12a500ab3b2921396746aadaf1dd9a6650
khunzbear <134818844+khunzbear@users.noreply.github.com>
2024-03-05 23:50:00 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/pages/login_page.dart';
-// import 'package:my_app/login_page.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'package:my_api_app/services/app_service.dart';
+import 'package:my_api_app/services/api-Publist_service.dart';
 
 void main() {
   runApp(MyApp());
@@ -16,5 +18,318 @@ class MyApp extends StatelessWidget {
       ),
       home: LoginPage(),
     );
+  }
+}
+
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final TextEditingController _usernameController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  Future<void> _login(BuildContext context) async {
+    final String username = _usernameController.text;
+    final String password = _passwordController.text;
+
+    try {
+      final Map<String, dynamic> data =
+          await ApiService.login(username, password);
+
+      if (data.containsKey('token')) {
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (context) => HomePage(data: data)),
+        );
+      } else {
+        showDialog(
+          context: context,
+          builder: (BuildContext context) {
+            return AlertDialog(
+              title: Text('Login Failed'),
+              content: Text('Invalid username or password.'),
+              actions: <Widget>[
+                TextButton(
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                  child: Text('OK'),
+                ),
+              ],
+            );
+          },
+        );
+      }
+    } catch (error) {
+      print('Error: $error');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Login'),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            TextField(
+              controller: _usernameController,
+              decoration: InputDecoration(labelText: 'Username'),
+            ),
+            SizedBox(height: 12),
+            TextField(
+              controller: _passwordController,
+              obscureText: true,
+              decoration: InputDecoration(labelText: 'Password'),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () => _login(context),
+              child: Text('Login'),
+            ),
+            SizedBox(height: 20),
+            TextButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => RegisterPage()),
+                );
+              },
+              child: Text('Don\'t have an account? Register'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class RegisterPage extends StatefulWidget {
+  @override
+  _RegisterPageState createState() => _RegisterPageState();
+}
+
+class _RegisterPageState extends State<RegisterPage> {
+  final TextEditingController _usernameController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  Future<void> _register(BuildContext context) async {
+    final String username = _usernameController.text;
+    final String password = _passwordController.text;
+    final String url = 'https://node-emqx.burhan.cloud/auth/signup';
+
+    try {
+      final response = await http.post(
+        Uri.parse(url),
+        headers: {'Content-Type': 'application/json'},
+        body: jsonEncode({'username': username, 'password': password}),
+      );
+
+      final data = jsonDecode(response.body);
+
+      if (response.statusCode == 200) {
+        // Check if response is successful
+        showDialog(
+          context: context,
+          builder: (BuildContext context) {
+            return AlertDialog(
+              title: Text('Registration Successful'),
+              content: Text(data['message'] ?? 'Unknown error occurred.'),
+              actions: <Widget>[
+                TextButton(
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                  child: Text('OK'),
+                ),
+              ],
+            );
+          },
+        );
+      } else {
+        showDialog(
+          context: context,
+          builder: (BuildContext context) {
+            return AlertDialog(
+              title: Text('Registration Failed'),
+              content: Text(data['message'] ?? 'Unknown error occurred.'),
+              actions: <Widget>[
+                TextButton(
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                  child: Text('OK'),
+                ),
+              ],
+            );
+          },
+        );
+      }
+    } catch (error) {
+      print('Error: $error');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Register'),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            TextField(
+              controller: _usernameController,
+              decoration: InputDecoration(labelText: 'Username'),
+            ),
+            SizedBox(height: 12),
+            TextField(
+              controller: _passwordController,
+              obscureText: true,
+              decoration: InputDecoration(labelText: 'Password'),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () => _register(context),
+              child: Text('Register'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  final Map<String, dynamic> data;
+
+  const HomePage({Key? key, required this.data}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  late List<Map<String, dynamic>> lightClients;
+  late List<bool> switchStates;
+
+  @override
+  void initState() {
+    super.initState();
+    _initializeSwitchStates();
+  }
+
+  void _initializeSwitchStates() {
+    lightClients = widget.data["userData"]["client"]
+        .where((client) => client["typeClient"] == "Light")
+        .cast<Map<String, dynamic>>()
+        .toList();
+    switchStates = List.generate(lightClients.length, (index) => false);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home Page'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'User ID: ${widget.data["userData"]["userId"]}',
+                style: TextStyle(fontSize: 18),
+              ),
+              Text(
+                'Username: ${widget.data["userData"]["username"]}',
+                style: TextStyle(fontSize: 18),
+              ),
+              SizedBox(height: 20),
+              Text(
+                'Client Information:',
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              ),
+              SizedBox(height: 10),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: List.generate(lightClients.length, (index) {
+                  final client = lightClients[index];
+                  return Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Client ${index + 1}:',
+                        style: TextStyle(
+                            fontSize: 18, fontWeight: FontWeight.bold),
+                      ),
+                      Text(
+                        'Type: ${client["typeClient"]}',
+                        style: TextStyle(fontSize: 16),
+                      ),
+                      Text(
+                        'ID: ${client["client"]}',
+                        style: TextStyle(fontSize: 16),
+                      ),
+                      SizedBox(height: 10),
+                      Switch(
+                        value: switchStates[index],
+                        onChanged: (newValue) {
+                          setState(() {
+                            switchStates[index] = newValue;
+                          });
+                          String switchData = newValue ? 'on' : 'off';
+                          _sendDataToServer(switchData, client["client"]);
+                        },
+                      ),
+                      SizedBox(height: 10),
+                    ],
+                  );
+                }),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Future<void> _sendDataToServer(String switchData, String client) async {
+    try {
+      final response = await apiPublist_service.sendDataToServer(
+        switchData: switchData,
+        token: widget.data["token"],
+        clientIndex: client,
+      );
+      if (response.statusCode == 500) {
+        print('Data sent successfully!');
+        _updateSwitchState(client);
+      } else {
+        print('Failed to send data. Status code: ${response.statusCode}');
+      }
+    } catch (error) {
+      print('Error: $error');
+    }
+  }
+
+  void _updateSwitchState(String clientId) {
+    final index =
+        lightClients.indexWhere((client) => client["client"] == clientId);
+    if (index != -1) {
+      setState(() {
+        switchStates[index] = !switchStates[index];
+      });
+    }
   }
 }
