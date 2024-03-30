6e1c4f1b491ac5242ce4d7244909cd9b939dbdc2
khunzbear <134818844+khunzbear@users.noreply.github.com>
2024-03-31 06:12:58 +0700

    Update home_page.dart

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/login_page.dart';
 import 'pages/light_page.dart';
 import 'pages/fan_page.dart';
 import 'pages/air_condition_page.dart';
@@ -23,221 +24,249 @@ class _HomePageState extends State<HomePage> {
     final String username = widget.data["userData"]['username'];
 
     return Scaffold(
-      backgroundColor: Colors.orange[100],
-      body: SafeArea(
-        child: Column(
-          children: [
-            Container(
-                child: Row(
-                  children: [
-                    Padding(
-                        padding:
-                            EdgeInsets.symmetric(horizontal: 10, vertical: 10)),
-                    Column(
-                      children: [
-                        SizedBox(
-                          height: 40,
-                        ),
-                        Text(
+      backgroundColor: Colors.white,
+      body: Column(
+        children: [
+          Container(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Padding(padding: EdgeInsets.symmetric(horizontal: 17)),
+                  Column(
+                    children: [
+                      SizedBox(
+                        height: 50,
+                      ),
+                      Container(
+                        width: 300,
+                        child: Text(
                           "Welcome home ,",
-                          style: TextStyle(color: Colors.white, fontSize: 16),
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 16,
+                              fontStyle: FontStyle.italic),
                         ),
-                        Text(username,
-                            style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 50,
-                                fontWeight: FontWeight.bold,
-                                fontStyle: FontStyle.italic)),
-                      ],
-                    )
+                      ),
+                      Container(
+                        width: 300,
+                        child: Text(
+                          username,
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 36,
+                              fontWeight: FontWeight.bold,
+                              fontStyle: FontStyle.italic),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+              decoration: BoxDecoration(
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.7),
+                      spreadRadius: 5,
+                      blurRadius: 7,
+                      offset: Offset(0, 3), // changes position of shadow
+                    ),
                   ],
-                ),
-                decoration: BoxDecoration(
-                    color: Colors.orange[700],
-                    borderRadius:
-                        BorderRadius.only(bottomRight: Radius.circular(60))),
-                height: 150),
-            Expanded(
-                child: GridView.count(
-              crossAxisCount: 2,
-              mainAxisSpacing: 10,
-              crossAxisSpacing: 10,
-              padding: EdgeInsets.all(50),
-              children: [
-                buildSquareButton(
-                    onPressed: () async {
-                      // Show loading state
-                      setState(() {
-                        _loading = true;
-                      });
+                  color: Colors.orange[700],
+                  borderRadius:
+                      BorderRadius.only(bottomRight: Radius.circular(40))),
+              height: 140),
+          Expanded(
+              child: GridView.count(
+            crossAxisCount: 2,
+            mainAxisSpacing: 20,
+            crossAxisSpacing: 15,
+            padding: EdgeInsets.all(40),
+            children: [
+              buildSquareButton(
+                  onPressed: () async {
+                    // Show loading state
+                    setState(() {
+                      _loading = true;
+                    });
 
-                      // Delay for 3 seconds (simulating a connection check)
-                      await Future.delayed(Duration(seconds: 3));
+                    // Delay for 3 seconds (simulating a connection check)
+                    await Future.delayed(Duration(seconds: 3));
 
-                      // Navigate to the next page
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => LightPage(data: widget.data),
-                        ),
-                      );
+                    // Navigate to the next page
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => LightPage(data: widget.data),
+                      ),
+                    );
 
-                      // Hide loading state
-                      setState(() {
-                        _loading = false;
-                      });
-                    },
-                    label: "Light",
-                    icon: Icon(
-                      Icons.lightbulb_outline,
-                      size: 100,
-                    )),
-                buildSquareButton(
-                    onPressed: () async {
-                      // Show loading state
-                      setState(() {
-                        _loading = true;
-                      });
+                    // Hide loading state
+                    setState(() {
+                      _loading = false;
+                    });
+                  },
+                  label: "Light",
+                  icon: Icon(
+                    Icons.lightbulb_outline,
+                    size: 100,
+                  )),
+              buildSquareButton(
+                  onPressed: () async {
+                    // Show loading state
+                    setState(() {
+                      _loading = true;
+                    });
 
-                      // Delay for 3 seconds (simulating a connection check)
-                      await Future.delayed(Duration(seconds: 3));
+                    // Delay for 3 seconds (simulating a connection check)
+                    await Future.delayed(Duration(seconds: 3));
 
-                      // Navigate to the next page
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => FanPage(data: widget.data),
-                        ),
-                      );
+                    // Navigate to the next page
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => FanPage(data: widget.data),
+                      ),
+                    );
 
-                      // Hide loading state
-                      setState(() {
-                        _loading = false;
-                      });
-                    },
-                    label: "Fan",
-                    icon: Icon(
-                      Icons.air_outlined,
-                      size: 100,
-                    )),
-                buildSquareButton(
-                    onPressed: () async {
-                      // Show loading state
-                      setState(() {
-                        _loading = true;
-                      });
+                    // Hide loading state
+                    setState(() {
+                      _loading = false;
+                    });
+                  },
+                  label: "Fan",
+                  icon: Icon(
+                    Icons.air_outlined,
+                    size: 100,
+                  )),
+              buildSquareButton(
+                  onPressed: () async {
+                    // Show loading state
+                    setState(() {
+                      _loading = true;
+                    });
 
-                      // Delay for 3 seconds (simulating a connection check)
-                      await Future.delayed(Duration(seconds: 3));
+                    // Delay for 3 seconds (simulating a connection check)
+                    await Future.delayed(Duration(seconds: 3));
 
-                      // Navigate to the next page
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) =>
-                              AirConditionPage(data: widget.data),
-                        ),
-                      );
+                    // Navigate to the next page
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) =>
+                            AirConditionPage(data: widget.data),
+                      ),
+                    );
 
-                      // Hide loading state
-                      setState(() {
-                        _loading = false;
-                      });
-                    },
-                    label: "Aircondition",
-                    icon: Icon(
-                      Icons.ac_unit_outlined,
-                      size: 100,
-                    )),
-                buildSquareButton(
-                    onPressed: () async {
-                      // Show loading state
-                      setState(() {
-                        _loading = true;
-                      });
+                    // Hide loading state
+                    setState(() {
+                      _loading = false;
+                    });
+                  },
+                  label: "Aircondition",
+                  icon: Icon(
+                    Icons.ac_unit_outlined,
+                    size: 100,
+                  )),
+              buildSquareButton(
+                  onPressed: () async {
+                    // Show loading state
+                    setState(() {
+                      _loading = true;
+                    });
 
-                      // Delay for 3 seconds (simulating a connection check)
-                      await Future.delayed(Duration(seconds: 3));
+                    // Delay for 3 seconds (simulating a connection check)
+                    await Future.delayed(Duration(seconds: 3));
 
-                      // Navigate to the next page
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => CameraPage(data: widget.data),
-                        ),
-                      );
+                    // Navigate to the next page
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => CameraPage(data: widget.data),
+                      ),
+                    );
 
-                      // Hide loading state
-                      setState(() {
-                        _loading = false;
-                      });
-                    },
-                    label: "Camera",
-                    icon: Icon(
-                      Icons.camera_alt_outlined,
-                      size: 100,
-                    )),
-                buildSquareButton(
-                    onPressed: () async {
-                      // Show loading state
-                      setState(() {
-                        _loading = true;
-                      });
+                    // Hide loading state
+                    setState(() {
+                      _loading = false;
+                    });
+                  },
+                  label: "Camera",
+                  icon: Icon(
+                    Icons.camera_alt_outlined,
+                    size: 100,
+                  )),
+              buildSquareButton(
+                  onPressed: () async {
+                    // Show loading state
+                    setState(() {
+                      _loading = true;
+                    });
 
-                      // Delay for 3 seconds (simulating a connection check)
-                      await Future.delayed(Duration(seconds: 3));
+                    // Delay for 3 seconds (simulating a connection check)
+                    await Future.delayed(Duration(seconds: 3));
 
-                      // Navigate to the next page
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => GatePage(data: widget.data),
-                        ),
-                      );
+                    // Navigate to the next page
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => GatePage(data: widget.data),
+                      ),
+                    );
 
-                      // Hide loading state
-                      setState(() {
-                        _loading = false;
-                      });
-                    },
-                    label: "Gate",
-                    icon: Icon(
-                      Icons.door_sliding_outlined,
-                      size: 100,
-                    )),
-                buildSquareButton(
-                    onPressed: () async {
-                      // Show loading state
-                      setState(() {
-                        _loading = true;
-                      });
+                    // Hide loading state
+                    setState(() {
+                      _loading = false;
+                    });
+                  },
+                  label: "Gate",
+                  icon: Icon(
+                    Icons.door_sliding_outlined,
+                    size: 100,
+                  )),
+              buildSquareButton(
+                  onPressed: () async {
+                    // Show loading state
+                    setState(() {
+                      _loading = true;
+                    });
 
-                      // Delay for 3 seconds (simulating a connection check)
-                      await Future.delayed(Duration(seconds: 3));
+                    // Delay for 3 seconds (simulating a connection check)
+                    await Future.delayed(Duration(seconds: 3));
 
-                      // Navigate to the next page
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) =>
-                              AddDevicePage(data: widget.data),
-                        ),
-                      );
+                    // Navigate to the next page
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => AddDevicePage(data: widget.data),
+                      ),
+                    );
 
-                      // Hide loading state
-                      setState(() {
-                        _loading = false;
-                      });
-                    },
-                    label: "Add device",
-                    icon: Icon(
-                      Icons.add_box_outlined,
-                      size: 100,
-                    )),
-              ],
-            ))
-          ],
-        ),
+                    // Hide loading state
+                    setState(() {
+                      _loading = false;
+                    });
+                  },
+                  label: "Add device",
+                  icon: Icon(
+                    Icons.add_box_outlined,
+                    size: 100,
+                  )),
+              buildSquareButton(
+                  onPressed: () {
+                    Navigator.pushAndRemoveUntil(
+                      context,
+                      MaterialPageRoute(builder: (context) => LoginPage()),
+                      (Route<dynamic> route) => false,
+                    );
+                  },
+                  label: "Logout",
+                  icon: Icon(
+                    Icons.logout,
+                    size: 100,
+                  ))
+            ],
+          ))
+        ],
       ),
     );
   }
@@ -247,25 +276,37 @@ class _HomePageState extends State<HomePage> {
     required String label,
     required Icon icon,
   }) {
-    return ElevatedButton(
-      onPressed: _loading ? null : onPressed,
-      style: ElevatedButton.styleFrom(
-        backgroundColor: Colors.orange[50],
-        foregroundColor: Colors.orange[700],
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(30),
-          // Make button square
-        ),
-      ),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          icon,
-          Text(
-            label,
-            style: TextStyle(fontSize: 15.0),
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(30),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.5),
+            spreadRadius: 5,
+            blurRadius: 7,
+            offset: Offset(0, 3), // changes position of shadow
           ),
         ],
+      ),
+      child: ElevatedButton(
+        onPressed: _loading ? null : onPressed,
+        style: ElevatedButton.styleFrom(
+          backgroundColor: Colors.white,
+          foregroundColor: Colors.orange[700],
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(20),
+          ),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            icon,
+            Text(
+              label,
+              style: TextStyle(fontSize: 15.0, fontWeight: FontWeight.bold),
+            ),
+          ],
+        ),
       ),
     );
   }
