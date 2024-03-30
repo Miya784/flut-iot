958a081d93296f8f19dcb2fa509c9522ab9eeba3
khunzbear <134818844+khunzbear@users.noreply.github.com>
2024-03-31 06:13:59 +0700

    Update register_page.dart

--- lib/pages/register_page.dart
+++ lib/pages/register_page.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
@@ -11,6 +10,9 @@ class RegisterPage extends StatefulWidget {
 class _RegisterPageState extends State<RegisterPage> {
   final TextEditingController _usernameController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
+  final TextEditingController _confirmPasswordController =
+      TextEditingController();
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
 
   Future<void> _register(BuildContext context) async {
     final String username = _usernameController.text;
@@ -27,7 +29,7 @@ class _RegisterPageState extends State<RegisterPage> {
       final data = jsonDecode(response.body);
 
       if (response.statusCode == 200) {
-        // Check if response is successful
+        // Registration successful
         showDialog(
           context: context,
           builder: (BuildContext context) {
@@ -38,6 +40,7 @@ class _RegisterPageState extends State<RegisterPage> {
                 TextButton(
                   onPressed: () {
                     Navigator.of(context).pop();
+                    Navigator.of(context).pop(); // Pop registration page
                   },
                   child: Text('OK'),
                 ),
@@ -46,6 +49,7 @@ class _RegisterPageState extends State<RegisterPage> {
           },
         );
       } else {
+        // Registration failed
         showDialog(
           context: context,
           builder: (BuildContext context) {
@@ -72,62 +76,115 @@ class _RegisterPageState extends State<RegisterPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.orange[100],
-      body: Padding(
-        padding: EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Icon(
-              Icons.add_home,
-              size: 150,
-              color: Colors.orange[700],
-            ),
-            SizedBox(height: 50),
-            Text(
-              'Please Enter a Username and password',
-              style: TextStyle(color: Colors.orange[700], fontSize: 16),
-            ),
-            SizedBox(height: 25),
-            TextField(
-              controller: _usernameController,
-              decoration: InputDecoration(
-                filled: true,
-                fillColor: Colors.orange[50],
-                focusedBorder: OutlineInputBorder(
-                    borderSide: BorderSide(width: 3, color: Colors.orange)),
-                hintText: 'Username',
-                border: OutlineInputBorder(),
-              ),
-            ),
-            SizedBox(height: 12),
-            TextField(
-              controller: _passwordController,
-              obscureText: true,
-              decoration: InputDecoration(
-                filled: true,
-                fillColor: Colors.orange[50],
-                focusedBorder: OutlineInputBorder(
-                    borderSide: BorderSide(width: 3, color: Colors.orange)),
-                hintText: 'Password',
-                border: OutlineInputBorder(),
-              ),
-            ),
-            SizedBox(height: 20),
-            Row(
-              children: [
-                Expanded(
-                  child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                        foregroundColor: Colors.orange[100],
-                        backgroundColor: Colors.orange[700]),
-                    onPressed: () => _register(context),
-                    child: Text('Register'),
+      backgroundColor: Colors.white,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.all(16.0),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Icon(
+                  Icons.add_home,
+                  size: 150,
+                  color: Colors.orange[700],
+                ),
+                SizedBox(height: 50),
+                Text(
+                  'Please Enter a Username and Password',
+                  style: TextStyle(
+                      color: Colors.orange[700],
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold),
+                ),
+                SizedBox(height: 25),
+                TextFormField(
+                  controller: _usernameController,
+                  decoration: InputDecoration(
+                    filled: true,
+                    fillColor: Colors.orange[50],
+                    focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(width: 3, color: Colors.orange)),
+                    hintText: 'Username',
+                    border: OutlineInputBorder(),
                   ),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter a username';
+                    }
+                    if (value.length < 6) {
+                      return 'Username must have at least 6 characters';
+                    }
+                    return null;
+                  },
+                ),
+                SizedBox(height: 12),
+                TextFormField(
+                  controller: _passwordController,
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    filled: true,
+                    fillColor: Colors.orange[50],
+                    focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(width: 3, color: Colors.orange)),
+                    hintText: 'Password',
+                    border: OutlineInputBorder(),
+                  ),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter a password';
+                    }
+                    return null;
+                  },
+                ),
+                SizedBox(height: 12),
+                TextFormField(
+                  controller: _confirmPasswordController,
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    filled: true,
+                    fillColor: Colors.orange[50],
+                    focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(width: 3, color: Colors.orange)),
+                    hintText: 'Confirm Password',
+                    border: OutlineInputBorder(),
+                  ),
+                  validator: (value) {
+                    if (value != _passwordController.text) {
+                      return 'Passwords do not match';
+                    }
+                    return null;
+                  },
+                ),
+                SizedBox(height: 20),
+                Row(
+                  children: [
+                    Expanded(
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          foregroundColor: Colors.orange[100],
+                          backgroundColor: Colors.orange[700],
+                          elevation: 10,
+                          shadowColor: Colors.grey,
+                        ),
+                        onPressed: () {
+                          if (_formKey.currentState!.validate()) {
+                            _register(context);
+                          }
+                        },
+                        child: Text(
+                          'Register now',
+                          style: TextStyle(
+                              color: Colors.white, fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
               ],
             ),
-          ],
+          ),
         ),
       ),
     );
