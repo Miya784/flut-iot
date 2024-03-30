999a730a2736e61d9bbfdbfb051d2da526434c18
khunzbear <134818844+khunzbear@users.noreply.github.com>
2024-03-31 06:12:11 +0700

    Update camera_page.dart

--- lib/pages/camera_page.dart
+++ lib/pages/camera_page.dart
@@ -23,7 +23,7 @@ class _CameraPageState extends State<CameraPage> {
 
   void _setupVlcControllers() {
     cameraClients = widget.data['userData']['client']
-        .where((client) => client['typeClient'] == 'camera')
+        .where((client) => client['typeClient'] == 'Camera')
         .toList();
 
     _vlcViewControllers = cameraClients.map((client) {
@@ -43,27 +43,108 @@ class _CameraPageState extends State<CameraPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Camera Page'),
-      ),
+      backgroundColor: Colors.orange[100],
       body: SingleChildScrollView(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            for (var i = 0; i < _vlcViewControllers.length; i++)
-              Padding(
-                padding: EdgeInsets.symmetric(vertical: 10),
-                child: Column(
+          children: [
+            Container(
+              padding: EdgeInsets.only(top: 40, left: 25, right: 10),
+              decoration: BoxDecoration(
+                  color: Colors.orange[700],
+                  borderRadius:
+                      BorderRadius.only(bottomLeft: Radius.circular(40)),
+                  boxShadow: [
+                    BoxShadow(
+                        color: Colors.grey.withOpacity(0.7),
+                        spreadRadius: 5,
+                        blurRadius: 7,
+                        offset: Offset(0, 3))
+                  ]),
+              child: SizedBox(
+                width: double.infinity,
+                height: 60,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    VlcPlayer(
-                      controller: _vlcViewControllers[i],
-                      aspectRatio: 16 / 9,
-                      placeholder: Center(child: CircularProgressIndicator()),
+                    Container(
+                      width: 40,
+                      height: 40,
+                      decoration: BoxDecoration(
+                          boxShadow: [
+                            BoxShadow(
+                                color: Colors.grey.withOpacity(0.7),
+                                spreadRadius: 5,
+                                blurRadius: 7,
+                                offset: Offset(0, 3))
+                          ],
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(100)),
+                      child: IconButton(
+                        onPressed: () {
+                          Navigator.pop(
+                              context); // This line ensures navigation back to the home page
+                        },
+                        icon: Icon(
+                          Icons.home,
+                          color: Colors.orange[700],
+                        ),
+                      ),
                     ),
-                    Text('Camera Clients: ${cameraClients[i]['client']}'),
+                    Text(
+                      "${widget.data["userData"]["username"]}'s cam",
+                      style: TextStyle(
+                          fontSize: 28,
+                          fontStyle: FontStyle.italic,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white),
+                    ),
                   ],
                 ),
               ),
+            ),
+            Container(
+              margin: EdgeInsets.symmetric(vertical: 20, horizontal: 10),
+              padding: EdgeInsets.all(20),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(10),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.withOpacity(0.5),
+                    spreadRadius: 3,
+                    blurRadius: 10,
+                    offset: Offset(0, 5), // changes position of shadow
+                  ),
+                ],
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  for (var i = 0; i < _vlcViewControllers.length; i++)
+                    Padding(
+                      padding: EdgeInsets.symmetric(vertical: 10),
+                      child: Column(
+                        children: [
+                          Text(
+                            'Cam name : ${cameraClients[i]['client']}',
+                            style: TextStyle(
+                                fontSize: 16,
+                                color: Colors.orange[900],
+                                fontWeight: FontWeight.bold,
+                                fontStyle: FontStyle.italic),
+                          ),
+                          VlcPlayer(
+                            controller: _vlcViewControllers[i],
+                            aspectRatio: 16 / 9,
+                            placeholder:
+                                Center(child: CircularProgressIndicator()),
+                          ),
+                        ],
+                      ),
+                    ),
+                ],
+              ),
+            ),
           ],
         ),
       ),
